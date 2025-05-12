690e7e735f438a9ba35732f06e88e0c4ec03ed54
subash26003 <subashcat67@gmail.com>
2025-05-12 14:29:13 +0530

    modified cors

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,18 @@ const server = http.createServer(app)
 const PORT = process.env.PORT
 
 app.use(express.json())
-app.use(cors())
+const allowedURLs = [process.env.HOD_PAGE_URL ,process.env.FACULTY_PAGE_URL , process.env.FACULTY_PAGE_URL ]
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 connectDB()
 
